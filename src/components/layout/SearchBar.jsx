c832ced60e424f2eed51e37880681f1ec2f7ec85
Helio Kanashiro <helio.fototerra@gmail.com>
2020-09-13 15:07:34 -0300

    Botão adicionar logs

--- src/components/layout/SearchBar.jsx
+++ src/components/layout/SearchBar.jsx
@@ -7,9 +7,9 @@ function SearchBar() {
                 <form>
                     <div className='input-field'>
                         <input id='search' type='search' />
-                        <label className='label-icon' htmlFor="search">
-                            <i className="material-icons">search</i>
-                            <i className="material-icons">close</i>
+                        <label className='label-icon' htmlFor='search'>
+                            <i className='material-icons'>search</i>
+                            <i className='material-icons'>close</i>
                         </label>
                     </div>
                 </form>
